e8b1792ac1f41077f21584a4eb33e76febd1879d
盛玉龙 <syl19920831@163.com>
2018-01-02 00:44:27 +0800

    客户端鉴权

--- app/Http/Controllers/ApiAuth/AuthTokenController.php
+++ app/Http/Controllers/ApiAuth/AuthTokenController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers\ApiAuth;
+
 use \App\Http\Controllers\Controller;
 
 use App\Http\Models\Backend\MembersModel;
@@ -7,89 +9,149 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Hash;
 
-class AuthTokenController extends Controller{
+class AuthTokenController extends Controller
+{
     protected static $_default_config = array(
         'host' => '127.0.0.1',
         'port' => 6379,
         'database' => 0
     );
+
     public function __construct(Request $request)
     {
         $this->request = $request;
         $this->_predis = new \Predis\Client([
             $config = array_merge(self::$_default_config, [
-                'host'=>env('REDIS_HOST'),
-                'port'=>env('REDIS_PORT'),
-                'database'=>env('REDIS_DATABASE',0)
+                'host' => env('REDIS_HOST'),
+                'port' => env('REDIS_PORT'),
+                'database' => env('REDIS_DATABASE', 0)
             ])
         ]);
     }
 
-    public function checkToken(){
+    public function checkToken()
+    {
 
         $access_token = $this->request->get('access_token');
 
-        $user_info  = $this->decode_access_token($access_token);
-        if(!$user_info){
+        $user_info = $this->decode_access_token($access_token);
+        if (!$user_info) {
             return [];
         }
         return $user_info;
     }
 
-    public function login(){
-        //
-        //$password = $this->request->get('user_name');
-        $open_type = $this->request->get('open_type') ? : 0;
+    public function login()
+    {
+        //1.接收第三方登录信息和open_id
+        $user_name = $this->request->get('user_name', '');
+        if (!$user_name) {
+            return response()->false('1314', '用户名不存在');
+        }
+        $avatar = $this->request->get('avatar', '');
+        $open_type = $this->request->get('open_type') ?: 1;
 
-        if(!$open_type || !in_array($open_type,[1,2,-1])){ //1 wechat 2 facebook
-            return response()->error('0001','不合法的应用关联');
+        if (!$open_type || !in_array($open_type, [1, 2, -1])) {
+            //1 wechat 2 facebook
+            return response()->error('0001', '不合法的应用关联');
         }
 
-        if($open_type == '2'){
-            $facebook_id = $this->request->get('facebook_open_id',0);
-            if(!$facebook_id){
-                return response()->error(0004,'没有facebook信息');
-            }
-            $user_info = $this->facebook_userinfo($facebook_id);
-        }elseif($open_type=='1'){
-            $wechat_open_id = $this->request->get('wechat_open_id',0);
-            if(!$wechat_open_id){
-                return response()->error(0005,'没有wechat信息');
-            }
-            // 插入数据
-            $user_info = $this->wechat_userinfo($wechat_open_id);
+        $open_id = $this->request->get('open_id', 0);
+        if (!$open_id) {
+            return response()->false('1413', 'open_id不存在');
+        }
 
+//        if ($open_type == '2') {
+//            $facebook_id = $this->request->get('facebook_open_id', 0);
+//            if (!$facebook_id) {
+//                return response()->error(0004, '没有facebook信息');
+//            }
+//            $user_info = $this->facebook_userinfo($facebook_id);
+//        } elseif ($open_type == '1') {
+//            $wechat_open_id = $this->request->get('wechat_open_id', 0);
+//            if (!$wechat_open_id) {
+//                return response()->error(0005, '没有wechat信息');
+//            }
+//            // 插入数据
+//            $user_info = $this->wechat_userinfo($wechat_open_id);
+//
+//            $user_info = [
+//                'member_id' => 1,
+//                'member_name' => 'zhanglong',
+//                'member_open_id' => 'sdasdasd',
+//                'source' => 1
+//            ];
+//        } else {
+//            $user_name = $this->request->get('user_name');
+//            $password = $this->request->get('password');
+//        }
+        $open_data = [
+            'open_type' =>  $open_type,
+            'open_id'   =>  $open_id,
+        ];
+        //2.查询用户是否已经存在
+        $exist_result = $this->_userExist($open_data);
+
+        //3.若果用户不存在,则插入数据库
+        if (!$exist_result) {
+            $member_regist = MembersModel::create([
+                'source'    =>  $open_type,
+                'open_id'   =>  $open_id,
+                'nick_name' =>  $user_name,
+                'avatar'    =>  $avatar
+            ])->toArray();
+            if (!$member_regist) {
+                return response()->false('1580', '登录失败');
+            }
+            $member_id = $member_regist['id'];
             $user_info = [
-                'member_id'=>1,
-                'member_name'=>'zhanglong',
-                'member_open_id'=>'sdasdasd',
-                'source'=>1
+                'source'    =>  $open_type,
+                'open_id'   =>  $open_id,
+                'nick_name' =>  $user_name,
+                'id'        =>  $member_id,
+                'avatar'    =>  $avatar,
             ];
-        }else{
-            $user_name = $this->request->get('user_name');
-            $password = $this->request->get('password');
+        } else {
+            $user_info = $exist_result[0];
         }
-        if(!$user_info){
-            return response()->error(0006,'未知的用户信息');
-        }
-        // 查询对应的用户是否需要存库 更新相应字段 是否允许登录
-        $access_token = $this->encode_access_token($user_info);
 
-        if($this->_predis->get($access_token)){
+        $access_token = $this->encode_access_token($user_info);
+//        $access_token = str_pad($access_token, 16);
+//        echo $access_token;die;
+        if ($this->_predis->get($access_token)) {
             $this->_predis->del($access_token);
         }
         $redis_bool = $this->_predis->setex(
             $this->get_token_key($access_token),
-            env('REDIS_EXPIRE_TIME',3600),
+            env('REDIS_EXPIRE_TIME', 3600),
             json_encode($user_info)
         );
-        if(!$redis_bool){
-            return response()->error(0007,'存储信息失效');
+        if (!$redis_bool) {
+            return response()->error(0007, '存储信息失效');
         }
         return response()->success([
-            'access_token'=>$access_token,
-            'expire_time'=>env('REDIS_EXPIRE_TIME')
+            'access_token' => $access_token,
+            'expire_time' => env('REDIS_EXPIRE_TIME'),
+            'user_id'       =>  $user_info['id'],
+            'user_name'     =>  $user_info['nick_name'],
+            'avatar'        =>  $avatar,
         ]);
+    }
+
+
+    private function _userExist($open_data)
+    {
+        if (!$open_data) {
+            return false;
+        }
+
+        $memeber_info = MembersModel::select('source', 'open_id', 'nick_name', 'id', 'avatar')
+            ->where('open_id', $open_data['open_id'])
+            ->where('source', $open_data['open_type'])
+            ->take(1)
+            ->get()
+            ->toArray();
+        return $memeber_info;
     }
 
     /**
@@ -97,14 +159,15 @@ class AuthTokenController extends Controller{
      * @param array $user_info
      * @return bool
      */
-    protected function encode_access_token($user_info=[]){
-        if(!$user_info){
+    protected function encode_access_token($user_info = [])
+    {
+        if (!$user_info) {
             return false;
         }
-        $time=time();
+        $time = time();
         $micro = microtime();
         $user_info = json_encode($user_info);
-        $sign = substr($time,-4,4).$user_info.substr($micro,4,4);
+        $sign = substr($time, -4, 4) . $user_info . substr($micro, 4, 4);
 
         $signature = Crypt::encrypt($sign);
 
@@ -116,14 +179,15 @@ class AuthTokenController extends Controller{
      * @param string $access_token
      * @return bool
      */
-    protected function decode_access_token($access_token=''){
+    protected function decode_access_token($access_token = '')
+    {
 
-        if(!$access_token){
+        if (!$access_token) {
             return false;
         }
         $signature = Crypt::decrypt($access_token);
-        $signature = substr($signature,4,-4);
-        $user_info = json_decode($signature,true);
+        $signature = substr($signature, 4, -4);
+        $user_info = json_decode($signature, true);
 
         return $user_info;
     }
@@ -133,14 +197,15 @@ class AuthTokenController extends Controller{
      * @param int $wechat_id
      * @return array
      */
-    protected function wechat_userinfo($wechat_id = 0){
-        if(!$wechat_id){
+    protected function wechat_userinfo($wechat_id = 0)
+    {
+        if (!$wechat_id) {
             return [];
         }
 
-        $memeber_info = MembersModel::where('open_id',$wechat_id)->where('source',1)->get()->toArray();
+        $memeber_info = MembersModel::where('open_id', $wechat_id)->where('source', 1)->get()->toArray();
         // 获取用户信息 并放入到用户信息表
-        if(!$memeber_info){
+        if (!$memeber_info) {
 
         }
         return $memeber_info;
@@ -152,15 +217,16 @@ class AuthTokenController extends Controller{
      * @param int $facebook_id
      * @return array
      */
-    protected function facebook_userinfo($facebook_id = 0 ){
-        if(!$facebook_id){
+    protected function facebook_userinfo($facebook_id = 0)
+    {
+        if (!$facebook_id) {
             return [];
         }
-
         // 获取用户信息 并放入到用户信息表
     }
 
-    protected function get_token_key($access_token){
-        return md5('user_'.$access_token);
+    protected function get_token_key($access_token)
+    {
+        return md5('user_' . $access_token);
     }
 }
