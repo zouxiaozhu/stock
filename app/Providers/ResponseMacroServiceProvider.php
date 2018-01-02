43a2f2b55698ada0a0a21e1fe97641e39e1c0d67
zouxiaozhu <1428804176@qq.com>
2018-01-03 00:01:21 +0800

    ''

--- app/Providers/ResponseMacroServiceProvider.php
+++ app/Providers/ResponseMacroServiceProvider.php
@@ -27,8 +27,8 @@ class ResponseMacroServiceProvider extends ServiceProvider
             return Response::json(['error_code' => $error_code, 'error_message' => $error_message], $status);
         });
 
-        Response::macro('false', function ($error_message = null,$error_code=400) {
-            return Response::json(['error_code' => $error_code, 'error_message' => $error_message,'status'=>false]);
+        Response::macro('false', function ($error_message = null,$error_code=400,$status=false) {
+            return Response::json(['error_code' => $error_code, 'error_message' => $error_message,$status]);
         });
     }
 
