81e2a443fa381b7bf1a038357793af887bee4863
zouxiaozhu <1428804176@qq.com>
2018-03-13 21:42:09 +0800

    ''

--- app/Repositories/ImplementsResp/SyncData.php
+++ app/Repositories/ImplementsResp/SyncData.php
@@ -423,7 +423,7 @@ class SyncData implements SyncDataInterface
     public function aceList($data)
     {
         $list = DB::table('ace')
-            ->select('id', 'product_type', 'to_price', 'action', 'stop_loss', 'avatar', 'create_user_name','create_time', 'date','time');
+            ->select('id', 'product_type', 'to_price', 'action', 'stop_loss', 'avatar', 'create_user_name','create_time', 'date','time', 'target');
         if (isset($data['type'])) {
             $list = $list->where('product_type','like', '%'.$data['type'].'%' );
         }
